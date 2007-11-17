e6dfcb310768969711f951ddf749878692c9fa82
Yossef <yossef@Cassady.local>
2007-11-17 10:12:00 -0600

    A bunch of returns inside a case statement? What is this parser written in, C?

--- lib/bot_parser.rb
+++ lib/bot_parser.rb
@@ -2,25 +2,30 @@ class BotParser
   def parse(sender, channel, mesg)
     return nil if mesg.empty?
     
-    result = { :poster => sender, :channel => channel }
-    case mesg 
+    common = { :poster => sender, :channel => channel }
+    
+    result = case mesg 
       when /^\s*(http:\S+\.(?:jpe?g|png|gif))(?:\s+(\S.*))?$/i
-        return result.merge(:type => 'image', :source => $1, :caption => ($2 || '') + " (posted by #{sender})")
+        { :type => 'image', :source => $1, :caption => ($2 || '') + " (posted by #{sender})" }
       when %r{^\s*(http://(?:www\.)?youtube\.com/\S+\?\S+)(?:\s+(.*))?$}i
-        return result.merge(:type => 'video', :embed => $1, :caption => ($2 || '') + " (posted by #{sender})")
+        { :type => 'video', :embed => $1, :caption => ($2 || '') + " (posted by #{sender})" }
       when /^\s*"([^"]+)"\s+--\s*(.*)\s+\((https?:.*)\)$/i
-        return result.merge(:type => 'quote', :quote => $1, :source => $2 + " (posted by #{sender})", :url => $3)
+        { :type => 'quote', :quote => $1, :source => $2 + " (posted by #{sender})", :url => $3 }
       when /^\s*"([^"]+)"\s+--\s*(.*)$/i
-        return result.merge(:type => 'quote', :quote => $1, :source => $2 + " (posted by #{sender})")
+        { :type => 'quote', :quote => $1, :source => $2 + " (posted by #{sender})" }
       when %r{^\s*(?:(.*?)\s+)?(https?://\S+)\s*(?:\s+(\S.*))?$}i
         title = $1 || Kernel::BotHelper.get_link_title($2)
-        return result.merge(:type => 'link', :url => $2, :name => title, :description => ($3 || '') + " (posted by #{sender})")
+        { :type => 'link', :url => $2, :name => title, :description => ($3 || '') + " (posted by #{sender})" }
       when %r{^\s*fact:\s+(.*)}i
-        return result.merge(:type => 'fact', :title => "FACT: #{$1}", :body => "(posted by #{sender})")
+        { :type => 'fact', :title => "FACT: #{$1}", :body => "(posted by #{sender})" }
       when %r{^\s*(?:(?:true\s+or\s+false)|(?:t\s+or\s+f))\s*[:\?]\s+(.*)}i
-        return result.merge(:type => 'true_or_false', :title => "True or False?  #{$1}", :body => "(posted by #{sender})")
-      else 
-        return nil
+        { :type => 'true_or_false', :title => "True or False?  #{$1}", :body => "(posted by #{sender})" }
+      else
+        nil
     end
+    
+    return nil unless result
+    
+    common.merge(result)
   end
 end
