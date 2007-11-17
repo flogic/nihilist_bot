57830428b445e8bd0bba28c813e7edc5af78652e
Yossef <yossef@Cassady.local>
2007-11-17 17:22:57 -0600

    Added add_poster_info helper (+ spec).

--- lib/bot_helper.rb
+++ lib/bot_helper.rb
@@ -16,4 +16,29 @@ module Kernel::BotHelper
     ''
   end
   module_function :get_link_title
+  
+  # Adds poster info to the appropriate field
+  def add_poster_info(info)
+    poster_info = "(posted by #{info[:poster]})"
+    
+    key = case info[:type]
+      when :image, :video : :caption
+      when :quote         : :source
+      when :link          : :description
+    end
+    
+    if key
+      info[key] ||= ''
+      info[key] += " #{poster_info}"
+    else
+      key = case info[:type]
+        when :fact, :true_or_false : :body
+      end
+      
+      info[key] = poster_info
+    end
+    
+    info
+  end
+  module_function :add_poster_info
 end
--- spec/bot_helper_spec.rb
+++ spec/bot_helper_spec.rb
@@ -5,6 +5,10 @@ describe Kernel::BotHelper do
   it 'should implement get_link_title as a module method' do
     Kernel::BotHelper.should respond_to(:get_link_title)
   end
+  
+  should 'implement add_poster_info as a module method' do
+    Kernel::BotHelper.should respond_to(:add_poster_info)
+  end
 end
 
 describe 'get_link_title', 'without a URL' do
@@ -42,4 +46,68 @@ describe 'get_link_title', 'with a URL' do
   # it 'should work if there is a network connection' do
   #   Kernel::BotHelper.get_link_title('http://ni.hili.st').should == 'ni.hili.st'
   # end
+end
+
+describe 'adding poster info' do
+  should 'require a hash' do
+    lambda { Kernel::BotHelper.add_poster_info }.should raise_error(ArgumentError)
+  end
+  
+  should 'accept a hash' do
+    lambda { Kernel::BotHelper.add_poster_info({}) }.should_not raise_error(ArgumentError)
+  end
+  
+  should 'add to caption for image' do
+    hash = { :type => :image, :poster => 'fred', :caption => 'test one two' }
+    result = Kernel::BotHelper.add_poster_info(hash)
+    result[:caption].should == 'test one two (posted by fred)'
+  end
+  
+  should 'handle nil caption for image' do
+    hash = { :type => :image, :poster => 'fred' }
+    result = Kernel::BotHelper.add_poster_info(hash)
+    result[:caption].should == ' (posted by fred)'
+  end
+    
+  should 'add to caption for video' do
+    hash = { :type => :video, :poster => 'fred', :caption => 'test one two' }
+    result = Kernel::BotHelper.add_poster_info(hash)
+    result[:caption].should == 'test one two (posted by fred)'
+  end
+  
+  should 'handle nil caption for video' do
+    hash = { :type => :video, :poster => 'fred' }
+    result = Kernel::BotHelper.add_poster_info(hash)
+    result[:caption].should == ' (posted by fred)'
+  end
+  
+  should 'add to source for quote' do
+    hash = { :type => :quote, :poster => 'fred', :source => 'test one two' }
+    result = Kernel::BotHelper.add_poster_info(hash)
+    result[:source].should == 'test one two (posted by fred)'
+  end
+  
+  should 'add to description for link' do
+    hash = { :type => :link, :poster => 'fred', :description => 'test one two' }
+    result = Kernel::BotHelper.add_poster_info(hash)
+    result[:description].should == 'test one two (posted by fred)'
+  end
+  
+  should 'handle nil description for link' do
+    hash = { :type => :link, :poster => 'fred' }
+    result = Kernel::BotHelper.add_poster_info(hash)
+    result[:description].should == ' (posted by fred)'
+  end
+  
+  should 'set body for fact' do
+    hash = { :type => :fact, :poster => 'fred', :body => 'test one two' }
+    result = Kernel::BotHelper.add_poster_info(hash)
+    result[:body].should == '(posted by fred)'
+  end
+  
+  should 'set body for true_or_face' do
+    hash = { :type => :true_or_false, :poster => 'fred', :body => 'test one two' }
+    result = Kernel::BotHelper.add_poster_info(hash)
+    result[:body].should == '(posted by fred)'
+  end
 end
