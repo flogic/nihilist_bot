1f54e5d025dfac546d10d89306ba8c149a9d7242
Yossef Mendelssohn <ymendel@pobox.com>
2007-11-26 13:43:44 -0600

    Modified spec to also use symbol-ized keys for options, not strings.

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -102,27 +102,27 @@ describe Bot, 'giving the sender configuration' do
   end
   
   should "fail unless a set of senders is known" do
-    @bot.stubs(:options).returns({ 'active_sender' => 'foo' })
+    @bot.stubs(:options).returns({ :active_sender => 'foo' })
     Proc.new { @bot.sender_configuration }.should raise_error(RuntimeError)
   end
   
   should "fail unless the specified active sender is known" do
-    @bot.stubs(:options).returns({ 'active_sender' => 'foo', 'senders' => { } })
+    @bot.stubs(:options).returns({ :active_sender => 'foo', :senders => { } })
     Proc.new { @bot.sender_configuration }.should raise_error(RuntimeError)    
   end
   
   should "fail unless the active sender has a destination type" do
-    @bot.stubs(:options).returns({ 'active_sender' => 'foo', 'senders' => { 'foo' => { } } })
+    @bot.stubs(:options).returns({ :active_sender => 'foo', :senders => { 'foo' => { } } })
     Proc.new { @bot.sender_configuration }.should raise_error(RuntimeError)        
   end
   
   should "succeed when options are fully specified" do
-    @bot.stubs(:options).returns({ 'active_sender' => 'foo', 'senders' => { 'foo' => { 'destination' => 'bar' } } })
+    @bot.stubs(:options).returns({ :active_sender => 'foo', :senders => { 'foo' => { 'destination' => 'bar' } } })
     Proc.new { @bot.sender_configuration }.should_not raise_error(RuntimeError)            
   end
   
   should "ensure that sender options are in a format usable by the sender" do
-    @bot.stubs(:options).returns({ 'active_sender' => 'foo', 'senders' => { 'foo' => { 'destination' => 'bar', 'option' => 'baz' } } })
+    @bot.stubs(:options).returns({ :active_sender => 'foo', :senders => { 'foo' => { 'destination' => 'bar', 'option' => 'baz' } } })
     result = @bot.sender_configuration
     result[:destination].should == :bar
     result[:option].should == :baz
