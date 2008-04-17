11443b682a1a6d83eeb64c6d413fe643ce25165b
Yossef Mendelssohn <ymendel@pobox.com>
2008-04-17 16:41:28 -0500

    Added filter to clean up HTML entities in link titles (using HTMLEntities gem)

---
+++ lib/filters/link_entity_cleanup.rb
@@ -0,0 +1,17 @@
+require File.join(File.dirname(__FILE__), 'template')
+$:.unshift(File.dirname(__FILE__)+'/../htmlentities/lib/')
+require 'htmlentities'
+
+class BotFilter::LinkEntityCleanup < BotFilter::Template
+  def process(data)
+    raise TypeError unless data.is_a?(Hash)
+    
+    return data unless data[:type] == :link
+    
+    result = data
+    if result[:name]
+      result[:name] = HTMLEntities.new.decode(result[:name])
+    end
+    result
+  end
+end
---
+++ spec/bot_filter_link_entity_cleanup_spec.rb
@@ -0,0 +1,74 @@
+require File.dirname(__FILE__) + '/spec_helper'
+require 'bot_filter'
+require 'filters/link_entity_cleanup'
+
+describe BotFilter::LinkEntityCleanup do
+  before :each do
+    @filter = BotFilter::LinkEntityCleanup.new
+  end
+  
+  it 'should accept options on initialization' do
+    lambda { BotFilter::LinkEntityCleanup.new(stub('options')) }.should_not raise_error(ArgumentError)
+  end
+  
+  it 'should not require options on initialization' do
+    lambda { BotFilter::LinkEntityCleanup.new }.should_not raise_error(ArgumentError)
+  end
+  
+  it 'should store options for this filter' do
+    options = { :filters => { 'link_entity_cleanup' => { :turd => :nugget } } }
+    filter = BotFilter::LinkEntityCleanup.new(options)
+    filter.options.should == { :turd => :nugget }
+  end
+  
+  it 'should default options to empty hash' do
+    filter = BotFilter::LinkEntityCleanup.new
+    filter.options.should == {}
+  end
+  
+  it 'should require data for processing' do
+    lambda { @filter.process }.should raise_error(ArgumentError)
+  end
+  
+  it 'should require a hash for processing' do
+    lambda { @filter.process('puppies') }.should raise_error(TypeError)
+  end
+  
+  it 'should accept a hash for processing' do
+    lambda { @filter.process({ :data => 'puppies'}) }.should_not raise_error
+  end
+  
+  it 'should return a hash' do
+    @filter.process({ :data => 'puppies'}).should be_kind_of(Hash)
+  end
+  
+  it 'should translate HTML entities in the name' do
+    result = @filter.process({ :name => '&gt;&amp;&lt;', :type => :link })
+    result[:name].should == '>&<'
+  end
+  
+  it 'should handle non--low-ASCII entities' do
+    result = @filter.process({ :name => '&raquo;&mdash;', :type => :link })
+    result[:name].should == '»—'
+  end
+  
+  it 'should handle numeric entities' do
+    result = @filter.process({ :name => '&#187;', :type => :link })
+    result[:name].should == '»'
+  end
+  
+  it 'should not affect non-entity text' do
+    result = @filter.process({ :name => 'hello there', :type => :link })
+    result[:name].should == 'hello there'
+  end
+  
+  it 'should not handle a combination of entity and non-entity text' do
+    result = @filter.process({ :name => 'hello &amp; there', :type => :link })
+    result[:name].should == 'hello & there'
+  end
+  
+  it 'should do nothing for non-links' do
+    result = @filter.process({ :name => '&gt; &amp; &lt;', :type => :blah })
+    result[:name].should == '&gt; &amp; &lt;'
+  end
+end
