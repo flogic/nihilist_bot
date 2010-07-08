f6f5092fec8f5d285941a322542a77ee2d199b48
Yossef Mendelssohn <ymendel@pobox.com>
2010-07-08 18:50:07 -0500

    Organized specs to mirror libs.
    Enough of these silly bot_filter_xxx_spec.rb filenames.

--- spec/bot_parser_format_spec.rb
+++ spec/bot_parser_format_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require File.expand_path(File.join(File.dirname(__FILE__), *%w[spec_helper]))
 require 'bot_parser_format'
 
 describe BotParserFormat, 'when initializing' do
