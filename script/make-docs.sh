4601ae95d79e8cb2220cdf9b39eeab42427d6caa
Yossef Mendelssohn <ymendel@pobox.com>
2010-07-09 14:48:31 -0500

    Removing Autumn Leaves cruft.
    We don't need all this crap anymore. It's cinch time.

--- script/make-docs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Generates the documentation for Autumn Leaves and your leaves. Documentation is
-# in HTML format and will be in the doc/ folder.
-
-rm -Rf doc
-rdoc -m README --title "Autumn Leaves Documentation" libs leaves README support genesis.rb big-switch.rb
