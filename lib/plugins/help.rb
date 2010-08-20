835af4dee47f1b45758d8083746956fb4e8f2c7c
Yossef Mendelssohn <ymendel@pobox.com>
2010-08-20 12:01:43 -0500

    Getting the help plugin *right*.
    Further spikes and testing have shown me how the new cinch plugins work.
      - listen_to really means listen and respond on every such event
      - match needs to be used for both regular and format-specific, which is fine
        - if I really wanted to, I'm sure I could have two matches and two methods

--- lib/plugins/help.rb
+++ lib/plugins/help.rb
@@ -1,21 +1,21 @@
 class BotPlugin::Help
   include Cinch::Plugin
   
-  listen_to :message
-  def listen(m)
-    formats = BotParser.formats
-    m.reply "Known formats: #{formats.collect { |f|  f.name }.join(', ')}"
-  end
+  match /^!help(?:\s+(\S+))?/, :use_prefix => false
   
-  match /^!help\s+(\S+)/, :use_prefix => false
   def execute(m, fmt)
     formats = BotParser.formats
-    format  = formats.detect { |f|  f.name == fmt.to_sym }
-    if format
-      description = format.description || 'no description available'
-      description.split("\n").each { |line|  m.reply "#{format.name}: #{line}" }
+    
+    if fmt
+      format = formats.detect { |f|  f.name == fmt.to_sym }
+      if format
+        description = format.description || 'no description available'
+        description.split("\n").each { |line|  m.reply "#{format.name}: #{line}" }
+      else
+        m.reply "Format '#{fmt}' unknown"
+      end
     else
-      m.reply "Format '#{fmt}' unknown"
+      m.reply "Known formats: #{formats.collect { |f|  f.name }.join(', ')}"
     end
   end
 end
--- spec/plugins/help_spec.rb
+++ spec/plugins/help_spec.rb
@@ -15,38 +15,8 @@ describe BotPlugin::Help do
     BotPlugin::Help.included_modules.should include(Cinch::Plugin)
   end
   
-  it 'should listen to messages' do
-    listener = BotPlugin::Help.instance_variable_get('@__cinch_listeners').first
-    listener.event.should  == :message
-    listener.method.should == :listen
-  end
-  
-  # triggered on plain !help
-  describe 'listening to a message' do
-    before :each do
-      @message = Struct.new(:nick, :channel, :text).new('somenick', 'somechannel', 'sometext')
-      @message.stubs(:reply)
-    end
-    
-    it 'should accept a message' do
-      lambda { @plugin.listen(@message) }.should_not raise_error(ArgumentError)
-    end
-    
-    it 'should require a message' do
-      lambda { @plugin.listen }.should raise_error(ArgumentError)
-    end
-    
-    it 'should get formats from parser' do
-      BotParser.expects(:formats).returns([])
-      @plugin.listen(@message)
-    end
-    
-    it 'should respond with a list of formats' do
-      formats = Array.new(3) { |i|  stub("format #{i}", :name => "format_#{i}".to_sym) }
-      BotParser.stubs(:formats).returns(formats)
-      @message.expects(:reply).with("Known formats: #{formats.collect { |f|  f.name }.join(', ')}")
-      @plugin.listen(@message)
-    end
+  it 'should not listen indiscriminately' do
+    BotPlugin::Help.instance_variable_get('@__cinch_listeners').should be_nil
   end
   
   it "should trigger execution on '!help [some format]' and keep the format name" do
@@ -56,60 +26,86 @@ describe BotPlugin::Help do
     pattern.method.should == :execute
   end
   
-  # triggered on !help [some format]
+  it "should trigger execution on '!help' and indicate no format name" do
+    pattern = BotPlugin::Help.instance_variable_get('@__cinch_patterns').first
+    pattern.pattern.match("!help")[1].should be_nil
+    pattern.method.should == :execute
+  end
+  
   describe 'executing a match' do
     before :each do
       @message = Struct.new(:nick, :channel, :text).new('somenick', 'somechannel', 'sometext')
       @message.stubs(:reply)
-      @format = 'link'
       
       @formats = Array.new(3) { |i|  stub("format #{i}", :name => "format_#{i}".to_sym, :description => "Description for format #{i}") }
       BotParser.stubs(:formats).returns(@formats)
     end
     
     it 'should accept a message and format' do
-      lambda { @plugin.execute(@message, @format) }.should_not raise_error(ArgumentError)
+      lambda { @plugin.execute(@message, 'some_format') }.should_not raise_error(ArgumentError)
     end
     
     it 'should require a message and format' do
       lambda { @plugin.execute(@message) }.should raise_error(ArgumentError)
     end
     
-    it 'should get formats from parser' do
-      BotParser.expects(:formats).returns([])
-      @plugin.execute(@message, @format)
-    end
-    
-    it 'should respond with format description' do
-      wanted_format = @formats[1]
-      @format = wanted_format.name.to_s
-      @message.expects(:reply).with("#{wanted_format.name}: #{wanted_format.description}")
-      @plugin.execute(@message, @format)
-    end
-    
-    it 'should respond with multiple lines if the format description is multiple lines' do
-      @formats[1].stubs(:description).returns("I have\nmany lines\nin my\ndescription")
-      wanted_format = @formats[1]
-      @format = wanted_format.name.to_s
-      wanted_format.description.split("\n").each do |desc_line|
-        @message.expects(:reply).with("#{wanted_format.name}: #{desc_line}")
+    describe 'when format is nil' do
+      before :each do
+        @format = nil
       end
-      @plugin.execute(@message, @format)
+      
+      it 'should get formats from parser' do
+        BotParser.expects(:formats).returns([])
+        @plugin.execute(@message, @format)
+      end
+
+      it 'should respond with a list of formats' do
+        @message.expects(:reply).with("Known formats: #{@formats.collect { |f|  f.name }.join(', ')}")
+        @plugin.execute(@message, @format)
+      end
     end
     
-    it 'should indicate an unspecified format description' do
-      @formats.each { |f|  f.stubs(:description).returns(nil) }
-      wanted_format = @formats[1]
-      @format = wanted_format.name.to_s
-      @message.expects(:reply).with("#{wanted_format.name}: no description available")
-      @plugin.execute(@message, @format)
-    end
-    
-    it 'should indicate an unknown format' do
-      wanted_format = 'turdnugget'
-      @format = wanted_format
-      @message.expects(:reply).with("Format '#{wanted_format}' unknown")
-      @plugin.execute(@message, @format)
+    describe 'when format is not nil' do
+      before :each do
+        @format = 'link'
+      end
+      
+      it 'should get formats from parser' do
+        BotParser.expects(:formats).returns([])
+        @plugin.execute(@message, @format)
+      end
+      
+      it 'should respond with format description' do
+        wanted_format = @formats[1]
+        @format = wanted_format.name.to_s
+        @message.expects(:reply).with("#{wanted_format.name}: #{wanted_format.description}")
+        @plugin.execute(@message, @format)
+      end
+      
+      it 'should respond with multiple lines if the format description is multiple lines' do
+        @formats[1].stubs(:description).returns("I have\nmany lines\nin my\ndescription")
+        wanted_format = @formats[1]
+        @format = wanted_format.name.to_s
+        wanted_format.description.split("\n").each do |desc_line|
+          @message.expects(:reply).with("#{wanted_format.name}: #{desc_line}")
+        end
+        @plugin.execute(@message, @format)
+      end
+      
+      it 'should indicate an unspecified format description' do
+        @formats.each { |f|  f.stubs(:description).returns(nil) }
+        wanted_format = @formats[1]
+        @format = wanted_format.name.to_s
+        @message.expects(:reply).with("#{wanted_format.name}: no description available")
+        @plugin.execute(@message, @format)
+      end
+      
+      it 'should indicate an unknown format' do
+        wanted_format = 'turdnugget'
+        @format = wanted_format
+        @message.expects(:reply).with("Format '#{wanted_format}' unknown")
+        @plugin.execute(@message, @format)
+      end
     end
   end
 end
